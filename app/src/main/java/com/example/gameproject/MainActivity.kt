f2518050e562bd28dd707525b364ba7f2710798f
11010127 <11010127@gm.chihlee.edu.tw>
2023-12-10 22:14:55 +0800

    處理跳頁頁面完成
    將內容上傳至github

--- app/src/main/java/com/example/gameproject/MainActivity.kt
+++ app/src/main/java/com/example/gameproject/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.gameproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.gameproject.databinding.ActivityMainBinding
@@ -10,5 +11,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.btSnake.setOnClickListener {
+            Intent(this@MainActivity,SnakeGame::class.java).apply {
+                startActivity(this)
+            }
+        }
+        binding.btPuzzle.setOnClickListener {
+            Intent(this@MainActivity,PuzzleVideoGame::class.java).apply {
+                startActivity(this)
+            }
+        }
     }
 }
