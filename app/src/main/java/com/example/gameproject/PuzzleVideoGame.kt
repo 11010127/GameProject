81e7c9317b92346109f3b8fb8006dd06124f2b77
11010127 <11010127@gm.chihlee.edu.tw>
2023-12-10 21:45:51 +0800

    建立2個頁面貪食蛇和益智遊戲
    設定給組員的遊戲頁面

---
+++ app/src/main/java/com/example/gameproject/PuzzleVideoGame.kt
@@ -0,0 +1,11 @@
+package com.example.gameproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PuzzleVideoGame : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_puzzle_video_game)
+    }
+}
