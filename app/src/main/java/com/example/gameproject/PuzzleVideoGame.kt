764f66ee286864a607fa7573abb04a46fe5be367
11010127 <11010127@gm.chihlee.edu.tw>
2023-12-12 23:58:42 +0800

    益智遊戲關卡首頁畫面完成

--- app/src/main/java/com/example/gameproject/PuzzleVideoGame.kt
+++ app/src/main/java/com/example/gameproject/PuzzleVideoGame.kt
@@ -1,11 +1,31 @@
 package com.example.gameproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
+import com.example.gameproject.databinding.ActivityPuzzleVideoGameBinding
 
 class PuzzleVideoGame : AppCompatActivity() {
+    private lateinit var binding: ActivityPuzzleVideoGameBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_puzzle_video_game)
+        binding= ActivityPuzzleVideoGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btn1.setOnClickListener {
+            Intent(this,MainActivity::class.java).apply {
+                startActivity(this)
+            }
+        }
+        binding.btn2.setOnClickListener {
+            Intent(this,MainActivity::class.java).apply {
+                startActivity(this)
+            }
+        }
     }
+
 }
