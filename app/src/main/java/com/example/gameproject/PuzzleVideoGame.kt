3bf5a1800e49509c37dda550d3ba840b72e76937
11010127 <11010127@gm.chihlee.edu.tw>
2023-12-17 01:55:11 +0800

    關卡頁面設定點擊按鈕顏色改變
    關卡頁面設定點擊按鈕顏色改變

--- app/src/main/java/com/example/gameproject/PuzzleVideoGame.kt
+++ app/src/main/java/com/example/gameproject/PuzzleVideoGame.kt
@@ -1,6 +1,7 @@
 package com.example.gameproject
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -19,51 +20,61 @@ class PuzzleVideoGame : AppCompatActivity() {
         binding.btn1.setOnClickListener {
             Intent(this,Game1::class.java).apply {
                 startActivity(this)
+                binding.btn1.setBackgroundColor(Color.RED)
             }
         }
         binding.btn2.setOnClickListener {
             Intent(this,Game2::class.java).apply {
                 startActivity(this)
+                binding.btn2.setBackgroundColor(Color.RED)
             }
         }
         binding.btn3.setOnClickListener {
             Intent(this,Game3::class.java).apply {
                 startActivity(this)
+                binding.btn3.setBackgroundColor(Color.RED)
             }
         }
         binding.btn4.setOnClickListener {
             Intent(this,Game4::class.java).apply {
                 startActivity(this)
+                binding.btn4.setBackgroundColor(Color.RED)
             }
         }
         binding.btn5.setOnClickListener {
             Intent(this,Game5::class.java).apply {
                 startActivity(this)
+                binding.btn5.setBackgroundColor(Color.RED)
             }
         }
         binding.btn6.setOnClickListener {
             Intent(this,Game6::class.java).apply {
                 startActivity(this)
+                binding.btn6.setBackgroundColor(Color.RED)
             }
         }
         binding.btn7.setOnClickListener {
             Intent(this,Game7::class.java).apply {
                 startActivity(this)
+                binding.btn7.setBackgroundColor(Color.RED)
             }
         }
         binding.btn8.setOnClickListener {
             Intent(this,Game8::class.java).apply {
                 startActivity(this)
+                binding.btn8.setBackgroundColor(Color.RED)
             }
         }
         binding.btn9.setOnClickListener {
             Intent(this,Game9::class.java).apply {
                 startActivity(this)
+                binding.btn9.setBackgroundColor(Color.RED)
             }
         }
         binding.btn10.setOnClickListener {
             Intent(this,Game10::class.java).apply {
                 startActivity(this)
+                binding.btn10.setBackgroundColor(Color.RED)
             }
         }
     }
