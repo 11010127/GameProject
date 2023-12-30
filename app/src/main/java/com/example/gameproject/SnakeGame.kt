e5e423139c66abcf89b87a06f32e529ec67d2f39
Lee <11010111@gm.chihlee.edu.tw>
2023-12-30 18:34:06 +0800

    貪食蛇主頁

--- app/src/main/java/com/example/gameproject/SnakeGame.kt
+++ app/src/main/java/com/example/gameproject/SnakeGame.kt
@@ -2,10 +2,21 @@ package com.example.gameproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.WindowManager
 
 class SnakeGame : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        val dm = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(dm)
+        Constants.SCREEN_WIDTH = dm.widthPixels
+        Constants.SCREEN_HEIGHT = dm.heightPixels
         setContentView(R.layout.activity_snake_game)
     }
 }
